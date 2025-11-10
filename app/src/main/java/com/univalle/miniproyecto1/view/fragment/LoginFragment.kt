de39d792f0378aca29a558af35966931b1e81c5c
jgarcia9300 <juan.m.garcia@correounivalle.edu.co>
2025-11-10 16:51:42 -0500

    creando HU 1

---
+++ app/src/main/java/com/univalle/miniproyecto1/view/fragment/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.univalle.miniproyecto1.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.univalle.miniproyecto1.R
+import com.univalle.miniproyecto1.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+   private lateinit var binding: FragmentLoginBinding
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //agregar funciones
+    }
+
+
+}
