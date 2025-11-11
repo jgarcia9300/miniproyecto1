cd63e5ecc52ff99a3c255182fa275783fe9c5bda
jgarcia9300 <juan.m.garcia@correounivalle.edu.co>
2025-11-11 16:32:41 -0500

    HU 2.0 finalizada

--- app/src/main/java/com/univalle/miniproyecto1/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/univalle/miniproyecto1/view/fragment/LoginFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.univalle.miniproyecto1.R
 import com.univalle.miniproyecto1.databinding.FragmentLoginBinding
+import java.util.concurrent.Executor
 
 @Suppress("DEPRECATION")
 class LoginFragment : Fragment() {
    private lateinit var binding: FragmentLoginBinding
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
 
 
@@ -27,6 +36,49 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //agregar funciones
+        login()
+    }
+
+    private fun login(){
+
+        executor = ContextCompat.getMainExecutor(requireContext())
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(context,
+                        "Autenticacion cancelada", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(context,
+                        "Autenticacion exitosa", Toast.LENGTH_SHORT)
+                        .show()
+                    findNavController().navigate(R.id.action_loginFragment_to_testFragment)
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(context, "Autenticacion fallida",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }})
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Autenticacion con biometria")
+            .setSubtitle("Ingrese su huella digital")
+            .setNegativeButtonText("Cancelar")
+            .build()
+
+
+        val login = binding.lottieFingerprint.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+
+        }
     }
 
 
